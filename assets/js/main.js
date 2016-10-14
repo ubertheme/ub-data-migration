5fbe856bc9353bfde337366ff9000f10a6cc5ede
qvv <quynhvv@joomsolutions.com>
2016-10-14 11:42:10 +0700

    Tuning

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,5 +19,28 @@ jQuery(function($){
 				});
 			};
 		});
+
+		//run slider with owl-carousel
+		jQuery('.owl-carousel').owlCarousel({
+			margin:10,
+			//rtl: true,
+			center:true,
+			//nav:true,
+			loop:true,
+			autoplay:true,
+			autoplayTimeout:3000,
+			autoplayHoverPause:true,
+			responsive:{
+				0:{
+					items:1
+				},
+				600:{
+					items:1
+				},
+				1000:{
+					items:1
+				}
+			}
+		});
 	});
 });
